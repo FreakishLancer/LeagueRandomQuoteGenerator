bc3cbf6209f803ecfcf5f1d85c8f238f60898617
FreakishLancer <destfinis@gmail.com>
2017-01-27 14:13:06 -0500

    2nd commit

--- js/quotes.js
+++ js/quotes.js
@@ -1,3 +1,537 @@
+const quoteData = {
+    "Aatrox": {
+        "quotes": ["Fight or be forgotten.", "Hesitation is death."],
+        "soundbites": ["http://vignette1.wikia.nocookie.net/leagueoflegends/images/2/27/Aatrox.attack04.ogg/revision/latest?cb=20130520212540",
+        "http://vignette2.wikia.nocookie.net/leagueoflegends/images/4/4a/Aatrox.move11.ogg/revision/latest?cb=20130520212747"]
+    },
+    "Akali": {
+        "quotes": ["Hesitation is the seed of defeat.", "We travel the same path."],
+        "soundbites": ["http://vignette3.wikia.nocookie.net/leagueoflegends/images/3/38/Akali.move7.ogg/revision/latest?cb=20121126174604",
+        "http://vignette2.wikia.nocookie.net/leagueoflegends/images/7/75/Akali.move9.ogg/revision/latest?cb=20121126174605"]
+    },
+    "Anivia": {
+        "quotes": ["Life passes so swiftly.", "Take time to reflect."],
+        "soundbites": ["http://vignette1.wikia.nocookie.net/leagueoflegends/images/b/b5/Anivia.attack02.ogg/revision/latest?cb=20130505111643",
+        "http://vignette2.wikia.nocookie.net/leagueoflegends/images/e/eb/Anivia.move05.ogg/revision/latest?cb=20130505112958"]
+    },
+    "Ashe": {
+        "quotes": ["Do not confuse mercy for weakness", "Peace requires a steady hand."],
+        "soundbites": ["http://vignette3.wikia.nocookie.net/leagueoflegends/images/2/20/Ashe.move03.ogg/revision/latest?cb=20130505134903",
+        "http://vignette2.wikia.nocookie.net/leagueoflegends/images/d/db/Ashe.move04.ogg/revision/latest?cb=20130505134911"]
+    },
+    "Aurelion Sol": {
+        "quotes": [],
+        "soundbites": []
+    },
+    "Azir": {
+        "quotes": [],
+        "soundbites": []
+    },
+    "Bard": {
+        "quotes": [],
+        "soundbites": []
+    },
+    "Blitzcrank": {
+        "quotes": [],
+        "soundbites": []
+    },
+    "Brand": {
+        "quotes": [],
+        "soundbites": []
+    },
+    "Braum": {
+        "quotes": [],
+        "soundbites": []
+    },
+    "Caitlyn": {
+        "quotes": [],
+        "soundbites": []
+    },
+    "Camille": {
+        "quotes": [],
+        "soundbites": []
+    },
+    "Cassiopeia": {
+        "quotes": [],
+        "soundbites": []
+    },
+    "Cho'Gath": {
+        "quotes": [],
+        "soundbites": []
+    },
+    "Corki": {
+        "quotes": [],
+        "soundbites": []
+    },
+    "Darius": {
+        "quotes": [],
+        "soundbites": []
+    },
+    "Diana": {
+        "quotes": [],
+        "soundbites": []
+    },
+    "Dr. Mundo": {
+        "quotes": [],
+        "soundbites": []
+    },
+    "Draven": {
+        "quotes": [],
+        "soundbites": []
+    },
+    "Ekko": {
+        "quotes": [],
+        "soundbites": []
+    },
+    "Elise": {
+        "quotes": [],
+        "soundbites": []
+    },
+    "Evelynn": {
+        "quotes": [],
+        "soundbites": []
+    },
+    "Ezreal": {
+        "quotes": [],
+        "soundbites": []
+    },
+    "Fiddlesticks": {
+        "quotes": [],
+        "soundbites": []
+    },
+    "Fiora": {
+        "quotes": [],
+        "soundbites": []
+    },
+    "Fizz": {
+        "quotes": [],
+        "soundbites": []
+    },
+    "Galio": {
+        "quotes": [],
+        "soundbites": []
+    },
+    "Gangplank": {
+        "quotes": [],
+        "soundbites": []
+    },
+    "Garen": {
+        "quotes": [],
+        "soundbites": []
+    },
+    "Gnar": {
+        "quotes": [],
+        "soundbites": []
+    },
+    "Gragas": {
+        "quotes": [],
+        "soundbites": []
+    },
+    "Graves": {
+        "quotes": [],
+        "soundbites": []
+    },
+    "Hecarim": {
+        "quotes": [],
+        "soundbites": []
+    },
+    "Heimerdinger": {
+        "quotes": [],
+        "soundbites": []
+    },
+    "Illaoi": {
+        "quotes": [],
+        "soundbites": []
+    },
+    "Irelia": {
+        "quotes": [],
+        "soundbites": []
+    },
+    "Ivern": {
+        "quotes": [],
+        "soundbites": []
+    },
+    "Janna": {
+        "quotes": [],
+        "soundbites": []
+    },
+    "Jarvan IV": {
+        "quotes": [],
+        "soundbites": []
+    },
+    "Jax": {
+        "quotes": [],
+        "soundbites": []
+    },
+    "Jayce": {
+        "quotes": [],
+        "soundbites": []
+    },
+    "Jhin": {
+        "quotes": [],
+        "soundbites": []
+    },
+    "Jinx": {
+        "quotes": [],
+        "soundbites": []
+    },
+    "Kalista": {
+        "quotes": [],
+        "soundbites": []
+    },
+    "Karma": {
+        "quotes": [],
+        "soundbites": []
+    },
+    "Karthus": {
+        "quotes": [],
+        "soundbites": []
+    },
+    "Kassadin": {
+        "quotes": [],
+        "soundbites": []
+    },
+    "Katarina": {
+        "quotes": [],
+        "soundbites": []
+    },
+    "Kayle": {
+        "quotes": [],
+        "soundbites": []
+    },
+    "Kennen": {
+        "quotes": [],
+        "soundbites": []
+    },
+    "Kha'Zix": {
+        "quotes": [],
+        "soundbites": []
+    },
+    "Kindred": {
+        "quotes": [],
+        "soundbites": []
+    },
+    "Kled": {
+        "quotes": [],
+        "soundbites": []
+    },
+    "Kog'Maw": {
+        "quotes": [],
+        "soundbites": []
+    },
+    "LeBlanc": {
+        "quotes": [],
+        "soundbites": []
+    },
+    "Lee Sin": {
+        "quotes": [],
+        "soundbites": []
+    },
+    "Leona": {
+        "quotes": [],
+        "soundbites": []
+    },
+    "Lissandra": {
+        "quotes": [],
+        "soundbites": []
+    },
+    "Lucian": {
+        "quotes": [],
+        "soundbites": []
+    },
+    "Lulu": {
+        "quotes": [],
+        "soundbites": []
+    },
+    "Lux": {
+        "quotes": [],
+        "soundbites": []
+    },
+    "Malphite": {
+        "quotes": [],
+        "soundbites": []
+    },
+    "Malzahar": {
+        "quotes": [],
+        "soundbites": []
+    },
+    "Maokai": {
+        "quotes": [],
+        "soundbites": []
+    },
+    "Master Yi": {
+        "quotes": [],
+        "soundbites": []
+    },
+    "Miss Fortune": {
+        "quotes": [],
+        "soundbites": []
+    },
+    "Mordekaiser": {
+        "quotes": [],
+        "soundbites": []
+    },
+    "Morgana": {
+        "quotes": [],
+        "soundbites": []
+    },
+    "Nami": {
+        "quotes": [],
+        "soundbites": []
+    },
+    "Nasus": {
+        "quotes": [],
+        "soundbites": []
+    },
+    "Nautilus": {
+        "quotes": [],
+        "soundbites": []
+    },
+    "Nidalee": {
+        "quotes": [],
+        "soundbites": []
+    },
+    "Nocturne": {
+        "quotes": [],
+        "soundbites": []
+    },
+    "Nunu": {
+        "quotes": [],
+        "soundbites": []
+    },
+    "Olaf": {
+        "quotes": [],
+        "soundbites": []
+    },
+    "Orianna": {
+        "quotes": [],
+        "soundbites": []
+    },
+    "Pantheon": {
+        "quotes": [],
+        "soundbites": []
+    },
+    "Poppy": {
+        "quotes": [],
+        "soundbites": []
+    },
+    "Quinn": {
+        "quotes": [],
+        "soundbites": []
+    },
+    "Rammus": {
+        "quotes": [],
+        "soundbites": []
+    },
+    "Rek'Sai": {
+        "quotes": [],
+        "soundbites": []
+    },
+    "Renekton": {
+        "quotes": [],
+        "soundbites": []
+    },
+    "Rengar": {
+        "quotes": [],
+        "soundbites": []
+    },
+    "Riven": {
+        "quotes": [],
+        "soundbites": []
+    },
+    "Rumble": {
+        "quotes": [],
+        "soundbites": []
+    },
+    "Ryze": {
+        "quotes": [],
+        "soundbites": []
+    },
+    "Sejuani": {
+        "quotes": [],
+        "soundbites": []
+    },
+    "Shaco": {
+        "quotes": [],
+        "soundbites": []
+    },
+    "Shen": {
+        "quotes": [],
+        "soundbites": []
+    },
+    "Shyvana": {
+        "quotes": [],
+        "soundbites": []
+    },
+    "Singed": {
+        "quotes": [],
+        "soundbites": []
+    },
+    "Sion": {
+        "quotes": [],
+        "soundbites": []
+    },
+    "Sivir": {
+        "quotes": [],
+        "soundbites": []
+    },
+    "Skarner": {
+        "quotes": [],
+        "soundbites": []
+    },
+    "Sona": {
+        "quotes": [],
+        "soundbites": []
+    },
+    "Soraka": {
+        "quotes": ["Every step; a new journey.", "Never waste a breath."],
+        "soundbites": ["http://vignette4.wikia.nocookie.net/leagueoflegends/images/1/16/Soraka.move6.ogg/revision/latest?cb=20121127210207",
+        "http://vignette3.wikia.nocookie.net/leagueoflegends/images/7/76/Soraka.move8.ogg/revision/latest?cb=20121127210208"]
+    },
+    "Swain": {
+        "quotes": [],
+        "soundbites": []
+    },
+    "Syndra": {
+        "quotes": [],
+        "soundbites": []
+    },
+    "Tahm Kench": {
+        "quotes": [],
+        "soundbites": []
+    },
+    "Taliyah": {
+        "quotes": [],
+        "soundbites": []
+    },
+    "Talon": {
+        "quotes": [],
+        "soundbites": []
+    },
+    "Taric": {
+        "quotes": [],
+        "soundbites": []
+    },
+    "Teemo": {
+        "quotes": [],
+        "soundbites": []
+    },
+    "Thresh": {
+        "quotes": [],
+        "soundbites": []
+    },
+    "Tristana": {},
+    "Trundle": {
+        "quotes": [],
+        "soundbites": []
+    },
+    "Tryndamere": {
+        "quotes": [],
+        "soundbites": []
+    },
+    "Twisted Fate": {
+        "quotes": [],
+        "soundbites": []
+    },
+    "Twitch": {
+        "quotes": [],
+        "soundbites": []
+    },
+    "Udyr": {
+        "quotes": [],
+        "soundbites": []
+    },
+    "Urgot": {
+        "quotes": [],
+        "soundbites": []
+    },
+    "Varus": {
+        "quotes": [],
+        "soundbites": []
+    },
+    "Vayne": {
+        "quotes": [],
+        "soundbites": []
+    },
+    "Veigar": {
+        "quotes": [],
+        "soundbites": []
+    },
+    "Vel'Koz": {
+        "quotes": [],
+        "soundbites": []
+    },
+    "Vi": {
+        "quotes": [],
+        "soundbites": []
+    },
+    "Viktor": {
+        "quotes": [],
+        "soundbites": []
+    },
+    "Vladimir": {
+        "quotes": [],
+        "soundbites": []
+    },
+    "Volibear": {
+        "quotes": [],
+        "soundbites": []
+    },
+    "Warwick": {
+        "quotes": [],
+        "soundbites": []
+    },
+    "Wukong": {
+        "quotes": [],
+        "soundbites": []
+    },
+    "Xerath": {
+        "quotes": [],
+        "soundbites": []
+    },
+    "Xin Zhao": {
+        "quotes": [],
+        "soundbites": []
+    },
+    "Yasuo": {
+        "quotes": [],
+        "soundbites": []
+    },
+    "Yorick": {
+        "quotes": [],
+        "soundbites": []
+    },
+    "Zac": {
+        "quotes": [],
+        "soundbites": []
+    },
+    "Zed": {
+        "quotes": [],
+        "soundbites": []
+    },
+    "Ziggs": {
+        "quotes": [],
+        "soundbites": []
+    },
+    "Zilean": {
+        "quotes": [],
+        "soundbites": []
+    },
+    "Zyra": {
+        "quotes": [],
+        "soundbites": []
+    }
+};
+
+
+
+
+
+
+
+
+
+
 class Champion {
     constructor(name) {
         this.name = name;
